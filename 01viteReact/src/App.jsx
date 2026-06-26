b5e80eaca57147a7befb588e96b21a9c40f85601
Shivanshu Mishra <mishrashivanshu908@gmail.com>
2026-06-26 23:51:00 +0530

    create custom react app lec-4

--- 01viteReact/src/App.jsx
+++ 01viteReact/src/App.jsx
@@ -1,8 +1,15 @@
 
 function App() {
-
+  const greet="Hello World!"
+  //{evaluated expresion only},no if else and other js syntax
   return (
-    <h1>Hello World!</h1>
+    <>
+      <h1>{greet}</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
+        fugiat?
+      </p>
+    </>
   )
 }
 
--- 01viteReact/src/main.jsx
+++ 01viteReact/src/main.jsx
@@ -1,11 +1,29 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Name from './myscript.jsx'
 
-createRoot(document.getElementById('root')).render(
+const anotherElement = (
   <>
-    <App />
-    <Name />
+    <h1>Hello World!</h1>
+    <a href="https://google.com" target="_blank">
+      Visit
+    </a>
   </>
+)
+const reactElement=React.createElement(
+  'a',
+  {href:'https://google.com', target:'_blank'},
+  '   click me to visit google'
+
+)
+const reactElement2 = React.createElement(
+  'a',
+  { href: 'https://facebook.com', target: '_blank' },
+  'click me to visit facebook',
+  reactElement
+)
+
+createRoot(document.getElementById('root')).render(
+  <App/>
 )
